9f2e57eb1875abdc10e27fdf496822414096da0e
andybunso <andybunso@hotmail.com>
2015-07-21 08:38:16 +0200

    Update ghscript.sh

--- ghscript.sh
+++ ghscript.sh
@@ -7,5 +7,5 @@ echo "test `date +%s`" >> index.html
 git init
 git add
 git commit -m "gh deploy"
-git push --force --quiet https://$GH_TOKEN@github.com/andybunso/travis-test.git master:gh-pages
+git push --force https://$GH_TOKEN@github.com/andybunso/travis-test.git master:gh-pages
 cd ..
