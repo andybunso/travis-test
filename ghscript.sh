de2b9f8138fd77176854bb36f359e9d49f785745
andybunso <andybunso@hotmail.com>
2015-07-21 08:40:33 +0200

    Update ghscript.sh

--- ghscript.sh
+++ ghscript.sh
@@ -5,6 +5,8 @@ cd test
 echo "test `date +%s`" >> index.html
 
 git init
+git config user.name "andy-travis"
+git config user.email "travis"
 git add .
 git commit -m "gh deploy"
 git push --force https://$GH_TOKEN@github.com/andybunso/travis-test.git master:gh-pages
